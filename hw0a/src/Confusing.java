2caa42186902a1355874569358794969bf95b5cb
zw0313 <19718997896@163.com>
2026-09-17 09:15:12 +0800

    完成了confusing

--- hw0a/src/Confusing.java
+++ hw0a/src/Confusing.java
@@ -31,5 +31,16 @@ public class Confusing {
 /* Before running the code, type your answer below.
 
 TODO: Write output here
-
+"I am method 1."
+"I am method 1."
+"I am method 2."
+"I am method 3."
+"I am method 1."
+"I am method 1."
+"I am method 2."
+"I am method 1."
+"I am method 2."
+"I am method 3."
+"I am method 1."
+method
 Then, click the green play button to check your work. */
