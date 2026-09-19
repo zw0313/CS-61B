eaaeac16619c703631267061c3bef74bd6ed4f19
zw0313 <19718997896@163.com>
2026-09-19 10:13:30 +0800

    完成ifElseMystery1

--- hw0a/src/IfElseMystery1.java
+++ hw0a/src/IfElseMystery1.java
@@ -30,5 +30,8 @@ public class IfElseMystery1 {
 /* Before running the code, type your answer below.
 
 TODO: Write output here
-
+13 21
+5 6
+6 5
+7 11
 Then, click the green play button to check your work. */
